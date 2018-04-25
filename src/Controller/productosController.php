13fb157ea279e1998ead289142524ef21cabb434
Alexis Pérez Llorca <alexis.perez.llorca@gmail.com>
2018-04-25 18:38:47 +0200

    'hoy'

--- src/Controller/productosController.php
+++ src/Controller/productosController.php
@@ -7,8 +7,13 @@
  */
 
 namespace App\Controller;
+use App\Entity\Producto;
+use App\Form\ProductoType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 
 class productosController extends Controller
 {
@@ -22,11 +27,35 @@ class productosController extends Controller
     }
 
     /**
-     *
+     *@return \Symfony\Component\HttpFoundation\Response
+     * @Route(path="/productos/formulario", name="app_form_producto")
      */
-    public function crear()
+    public function crearAccion()
     {
+        $action = $this->generateUrl('app_producto_creando');
+        $product = new Producto();
+        $form = $this->createForm(ProductoType::class, $product);
 
+        return $this->render('crearproducto.html.twig',
+            ['action' => $action,
+                'form' => $form->createView()]
+        );
+    }
+
+    /**
+     *@return \Symfony\Component\HttpFoundation\Response
+     * @Route(path="/productos/crear", name="app_producto_creando")
+     */
+    public function hacerCrearAccion(EntityManagerInterface $em, Request $request)
+    {
+        $producto = new Producto();
+        $formulario = $this->createForm(ProductoType::class, $producto);
+        $formulario->handleRequest($request);
+        if ($formulario->isValid()) {
+            $em->persist($producto);
+            $em->flush();
+            return $this->redirectToRoute('app_producto');
+        }
     }
 
 }
